c9c4909e01a5f29adf8e7aea2aa9804c72f2b446
Chinmay Shastri <chinmay.shastri@digitalturbine.com>
2024-07-02 09:25:55 +0530

    commit the changes

--- create_instance /variables.tf
+++ create_instance /variables.tf
@@ -12,8 +12,8 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map
   default = {
-    us-east-1 = "ami-04b3c39a8a1c62b76"
-    us-east-2 = "ami-0b76100074ce446d4"
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-east-2 = "ami-05803413c51f242b7"
   }
 }
 
