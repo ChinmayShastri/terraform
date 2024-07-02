e4fc8aa571b73312c2f0faf5c3d24123e15cca0c
Chinmay Shastri <chinmay.shastri@digitalturbine.com>
2024-07-02 21:30:16 +0530

    updated the code

--- userdata/userdata.tf
+++ userdata/userdata.tf
@@ -1,11 +1,11 @@
 # Provider Configuration
 provider "aws" {
-  region = "us-east-1"  # Specify the desired AWS region
+  region = "us-east-1a"  # Specify the desired AWS region
 }
 
 # EC2 Instance with User Data
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI
+  ami           = "ami-0b0ea68c435eb488d"  # Replace with your desired AMI
   instance_type = "t2.micro"
 
   user_data = <<-EOF
